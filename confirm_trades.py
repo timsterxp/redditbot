73527154156625eea426f776f24990bb64a1f7ea
Tim Nguyen <timmyn97@gmail.com>
2020-11-26 14:02:37 -0800

    Added code to validate one of the users has posted recently

--- confirm_trades.py
+++ confirm_trades.py
@@ -10,9 +10,9 @@ import time
 import mysql.connector
 
 connection = mysql.connector.connect(host='HOSTADDRESS',database='DATABASENAME',user='USERNAME',password='PASSWORD')
-reddit = praw.Reddit('BOTNAME/CONFIG')
+submission = reddit.submission(id='SUBMISSIONID')
 subreddit=reddit.subreddit('SUBREDDITNAME')
-submission = reddit.submission(id="SUBMISSIONID")
+reddit = praw.Reddit('BOTNAME/CONFIG')
 fileName="filename.txt"
 
 #Finds the user and increments trade count, if does not exist, create a new user in MySQL
@@ -39,14 +39,14 @@ def incrementTrades(username):
 
 #Add a new user into MySQL
 def addToDatabase(username):
-    sql_add_user="insert into trades values('"+username+"','1')"
+    sql_add_user="insert into trades(name,confirmed) values('"+username+"','1')"
     print("added "+username + " to database")
     cursor=connection.cursor()
     cursor.execute(sql_add_user)
     connection.commit()
     updateFlair(username)
 
-#Update User Flair after incrementing/adding to database
+#Update User flair after incrementing/adding to database
 def updateFlair(username):
     cursor=connection.cursor()
     sql_get_count="select confirmed from trades where name='"+username+"'"
@@ -54,13 +54,24 @@ def updateFlair(username):
     record=cursor.fetchone()
     subreddit.flair.set(username, text="Trades:"+str(record[0]),css_class="")
     
+def checkUsers(userOne,userTwo):
+    cursor=connection.cursor()
+    sql_check_users="select posted from trades where name ='"+userOne+"' or name='"+userTwo+"'"
+    cursor.execute(sql_check_users)
+    userList=cursor.fetchall()
+    valNeeded =0;
+    for val in userList:
+        valNeeded+=val[0]
+    return valNeeded
+
+
 # Have we run this code before? If not, create an empty list
 if not os.path.isfile(fileName):
     posts_replied_to = []
     # If we have run the code before, load the list of posts we have replied to
 else:
     # Read the file into a list and remove any empty values
-    with open("FILENAME", "r") as f:
+    with open(fileName, "r") as f:
         posts_replied_to = f.read()
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = list(filter(None, posts_replied_to))
@@ -81,12 +92,15 @@ for top_level_comment in submission.comments:
             #User replying must have been tagged in original comment
             if (replyUser in top_level_comment.body):
                 posts_replied_to.append(second_level_comment.parent_id)
-                #search for users
-                findUser(topUser)
-                findUser(replyUser)
+                #Ensure that at least one of the users had posted in the past month
+                if checkUsers(topUser,replyUser)!=0:
+                     #search for users
+                    findUser(topUser)
+                    findUser(replyUser)
                 #Give confirmation to users that it worked
-                second_level_comment.reply("Added")
-            
+                    second_level_comment.reply("Added")
+                else:
+                    top_level_comment.reply("There was an issue adding, paging /u/ItzADino")
 with open(fileName, "w") as f:
     for post_id in posts_replied_to:
         f.write(post_id + "\n")
