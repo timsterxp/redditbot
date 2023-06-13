e240a3ee9bc9b7c6a1191a318a2c10332d93459c
Tim Nguyen <timmyn97@gmail.com>
2023-06-12 23:36:32 -0700

    1.01, minor name changes

--- modmail_test.py
+++ modmail_test.py
@@ -13,7 +13,7 @@ import time
 
 reddit = praw.Reddit('BOTNAME/CONFIG')
 subreddit=reddit.subreddit('SUBREDDITNAME')
-fileName = fileNameHere
+fileName = "mail_replied_to.txt"
 
 if not os.path.isfile(fileName):
     mail_replied_to = []
@@ -26,9 +26,14 @@ else:
         mail_replied_to = mail_replied_to.split("\n")
         mail_replied_to = list(filter(None, mail_replied_to))
         
-conversations = subreddit.modmail.conversations(state="new")
+conversations = subreddit.modmail.conversations(state="mod")
         
 for eachConvo in conversations:
+    if (eachConvo.num_messages>1):
+        print ("There is more than 1 message! We found: ")
+        print(eachConvo.num_messages)
+        #A secondary check to ensure we arent spamming users
+        continue
     if (eachConvo.id not in mail_replied_to):
         print ("Yes this was not found previously!")
         #Add it to the list before doing any actions 
