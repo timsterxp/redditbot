972784c9f54e768b2758038e0785becb2a5135e8
Tim Nguyen <timmyn97@gmail.com>
2023-06-12 22:17:42 -0700

    Testing modmail automating

---
+++ modmail_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3.7
+#Removed MySQL info/Subreddit Details for Github Usage/File Example
+
+import praw
+import pdb
+import re
+import os
+import datetime
+import time
+import mysql.connector
+
+
+subreddit=reddit.subreddit('SUBREDDITNAME')
+reddit = praw.Reddit('BOTNAME/CONFIG')
+#fileName = fileName
+
+conversations = reddit.subreddit(subreddit).modmail.conversations(state="mod")
+for message in conversation.messages:
+    print(message.body_markdown)
+    
+    
